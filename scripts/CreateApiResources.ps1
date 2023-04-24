9d26eb29854ecda11b49cfa046fb2407c85f9744
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-04-24 22:52:51 +0200

    updated build and publish steps

--- scripts/CreateApiResources.ps1
+++ scripts/CreateApiResources.ps1
@@ -5,4 +5,3 @@ $appServiceName = "marvellous-api"
 az group create --name $resourceGroup --location $location                                                                                        
 az appservice plan create --name $appServicePlanName  --resource-group $resourceGroup --sku free                                               
 az webapp create --name $appServiceName --resource-group $resourceGroup --plan $appServicePlanName                                        
-az webapp deploy --resource-group $resourceGroup --name $appServiceName --src-path .\deploy\marvellousApi.zip --type zip --async true
