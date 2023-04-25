87e47030000418616509c3ec29ea6f34bb192335
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-04-25 22:38:31 +0200

    updated to add env variables

--- MarvellousApi/MarvellousApi/Program.cs
+++ MarvellousApi/MarvellousApi/Program.cs
@@ -3,8 +3,9 @@ using MarvellousApi.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 var env = builder.Environment;
 builder.Configuration.SetBasePath(env.ContentRootPath);
-builder.Configuration.AddJsonFile("appsettings.json", false, true);
-builder.Configuration.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+builder.Configuration.AddJsonFile("appsettings.json", true, true);
+builder.Configuration.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+builder.Configuration.AddEnvironmentVariables();
 
 
 // Add services to the container.
